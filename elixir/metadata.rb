df4b90dad5a5683f5b70581a988f8c459f68699a
Sean Stavropoulos <sean.stavro@gmail.com>
2016-01-21 12:50:01 -0800

    initial commit

---
+++ elixir/metadata.rb
@@ -0,0 +1,7 @@
+name 'elixir'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures elixir'
+long_description 'Installs/Configures elixir'
+version '0.1.0'
---
+++ elixir/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: elixir
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+file "#{ENV['HOME']}/test.txt" do
+  content 'This file was created by Chef!'
+end
